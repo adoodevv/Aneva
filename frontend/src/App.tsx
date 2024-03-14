ec692252b5a8b818912f2b07195a52b094eb0ae4
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-03-14 19:45:00 +0000

    Style enhancement

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,9 @@ import FileUpload from './components/FileUpload';
 
 const App: React.FC = () => {
   return (
-    <div className="">
-      <h1>Aneva</h1>
-      <div className="">
+    <div className="container mx-auto p-4 flex flex-col items-center">
+      <h1 className="text-4xl font-bold mb-8 text-center">Aneva</h1>
+      <div className="shadow-md rounded-lg">
         <FileUpload />
       </div>
       <FileList />
