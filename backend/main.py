7399337137d36f410e9615639a31801881d6d9fd
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-03-23 21:00:36 +0000

    restructuring as package to run bug fix

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,16 @@
-from typing import Union
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .api.endpoints import files
 
 app = FastAPI()
 
+#CORS middleware
+app.add_middleware(
+   CORSMiddleware,
+   allow_origins=["http://localhost:5173"],
+   allow_credentials=True,
+   allow_methods=["*"],
+   allow_headers=["*"],
+)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(files.router)
